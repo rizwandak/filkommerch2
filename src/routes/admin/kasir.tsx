45956d2d5d411f446ece624cc011daf867096276
rizwandakeysha <rizwandakeysha@gmail.com>
2026-07-05 21:58:40 +0700

    clean: hapus folder src root yang usang

--- src/routes/admin/kasir.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/admin/kasir")({
-  beforeLoad: () => {
-    throw redirect({ to: "/pos" });
-  },
-});
